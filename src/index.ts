12188076dba3a367153b41ea8eb6965207e295b9
gyan <gyan@letslooksie.com>
2023-02-27 17:31:57 +0530

    use try and catch

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,29 @@
 import axios from "axios";
 export class Retrieve{
     static async getData(){
-        const url = "https://jsonplaceholder.typicode.com/posts"
-        return await axios({
-          method: 'get',
-          url: url,
-        })
-      .then(async response => {
-        console.log("response here man",response)
-        if (!response.data) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return await response;
-      })
-      .catch(error => {
-        console.error(`Error retrieving data from ${url}: ${error}`);
-      });
+      //   const url = "https://jsonplaceholder.typicode.com/posts"
+      //   return await axios({
+      //     method: 'get',
+      //     url: url,
+      //   })
+      // .then(async response => {
+      //   console.log("response here man",response)
+      //   if (!response.data) {
+      //     throw new Error(`HTTP error! Status: ${response.status}`);
+      //   }
+      //   return await response;
+      // })
+      // .catch(error => {
+      //   console.error(`Error retrieving data from ${url}: ${error}`);
+      // });
+
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        return response.data;
+      } catch (error) {
+        return error;
+        console.error(error);
+      }
 
     }
 };
