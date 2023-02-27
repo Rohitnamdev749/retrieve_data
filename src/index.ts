4992239307c02804f36a114f729f68f9cdd84936
gyan <gyan@letslooksie.com>
2023-02-27 16:14:11 +0530

    change axios object for get

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,12 @@ import axios from "axios";
 export class Retrieve{
     static async getData(){
         const url = "https://jsonplaceholder.typicode.com/posts"
-        return await axios.get(url)
+        return await axios({
+          method: 'get',
+          url: url,
+        })
       .then(async response => {
+        console.log("response here man",response)
         if (!response.data) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
